005d21bee83fc2170d2e4b8ea677e8af81dbdd46
Koome <judekoome360@gmail.com>
2023-04-27 09:26:56 +0300

    Complete

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 
-function Form(props) {
-  const [firstName, setFirstName] = useState("Sylvia");
-  const [lastName, setLastName] = useState("Woods");
+function Form() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [submittedData, setSubmittedData] = useState([]);
+  const [errors, setErrors] = useState([]);
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,12 +14,57 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    // Validation logic
+    if (firstName.length === 0 || lastName.length === 0) {
+      setErrors(["Please enter both first and last name"]);
+      return;
+    }
+
+    const formData = { firstName: firstName, lastName: lastName };
+    const dataArray = [...submittedData, formData];
+    setSubmittedData(dataArray);
+    setFirstName("");
+    setLastName("");
+    setErrors([]);
+  }
+
+  const listOfSubmissions = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    );
+  });
+
+  const listOfErrors = errors.map((error, index) => {
+    return <div key={index}>{error}</div>;
+  });
+
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={handleFirstNameChange}
+          value={firstName}
+          placeholder="First Name"
+        />
+        <input
+          type="text"
+          onChange={handleLastNameChange}
+          value={lastName}
+          placeholder="Last Name"
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <h3>Submissions</h3>
+      {listOfSubmissions}
+      <h3>Errors</h3>
+      {listOfErrors}
+    </div>
   );
 }
 
